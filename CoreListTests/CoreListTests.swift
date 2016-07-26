a15eacacdccb18b4cbf3021f8daec3ab3a2be95e
mohaiminul-sust <mohaiminul.sust@csesociety.org>
2016-07-26 21:57:02 +0600

    Initial Commit

---
+++ CoreListTests/CoreListTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoreListTests.swift
+//  CoreListTests
+//
+//  Created by ANDROMEDA on 7/26/16.
+//  Copyright © 2016 infancyit. All rights reserved.
+//
+
+import XCTest
+@testable import CoreList
+
+class CoreListTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
